c52f50094e5c7c1ba3dc51c04640c250a0245cb4
Paula <paulavimf@gmail.com>
2021-02-05 11:07:00 +0000

    new helper for plotting overlayed time series in a period

--- helpers/plot_helpers.py
+++ helpers/plot_helpers.py
@@ -1,7 +1,24 @@
 import matplotlib.pyplot as plt
 
-def overlay_timeseries(TitileDataDict):
-    for k,v in TitileDataDict:
-        ax = v[0].plot(color=v[1], grid=True, label=k)
-        # TODO terminar esto para que acepte el dash y tal... mirar el tema de args y kwargs y hacerlo e otra forma
-        pass
+
+'''
+period  = [ 'init' , 'end']
+env_data = env_data
+data_to_compare = [data1,data2,data2..]
+title = [title1,title2,title3]
+'''
+
+
+def overlay_timeseries(p, env_data, data_list, titles):
+    plt.figure(figsize=(12, 5))
+    ax = []
+
+    for i, data in enumerate(data_list):
+        ax.append(data[p[0]:p[1]].plot(grid=True, label=titles[i]))
+    ax.append(
+        env_data[p[0]:p[1]].plot(linestyle='--', color='black', grid=True, secondary_y=True, label='Env Temperature'))
+
+    h1, l1 = ax[-1].get_legend_handles_labels()
+    h2, l2 = ax[-2].get_legend_handles_labels()
+    # TODO esta movida del handles labels no sé cómo es
+    plt.legend(h1 + h2, l1 + l2, loc=0)
