8c439d30dae41d7effd25f23e9e66fa98957c591
Paula <paulavimf@gmail.com>
2021-02-10 17:29:54 +0000

    new note book organization and refacto

---
+++ studies/TemperatureStudy3_1.py
@@ -0,0 +1,28 @@
+from helpers.idf_helpers import *
+from studies import run_study
+
+# SIMPLE ROOM with people and no AIR CONDITIONER,
+'''
+__TemperatureStudy3__:
+
+- PEOPLE: SÍ
+- LIGHTS: SÍ
+- ELECTRICEQUIPMENT: SÍ
+- AIRCONDITIONER: NO
+
+              '''
+
+
+def main():
+    idffile = '/home/paula/Documentos/Doctorado/Desarrollo/eppyProject/input/simple_room_1_window_1_door.idf'
+    iddfile = '/usr/local/EnergyPlus-9-4-0/Energy+.idd'
+    epwfile = '/home/paula/Documentos/Doctorado/Desarrollo/eppyProject/input/Wheather file/FRA_Paris.Orly.071490_IWEC.epw'
+
+    idf = initialization(idffile, iddfile, epwfile)
+    objs_to_delete = []
+    vars_name = ['Site Outdoor Air Drybulb Temperature', 'Zone Air Temperature']
+    run_study(idf, objs_to_delete, vars_name, os.path.basename(__file__))
+
+
+if __name__ == '__main__':
+    main()
--- studies/__init__.py
+++ studies/__init__.py
@@ -9,10 +9,11 @@ def run_study(idf, objs_to_delete, vars_name, scriptname,filename = os.getcwd())
     for n in range(len(vars_name)):
         idf.newidfobject('OUTPUT:VARIABLE')
         idf.idfobjects['OUTPUT:VARIABLE'][n].Variable_Name = vars_name[n]
-    schedule_name = idf.idfobjects['HVACTemplate:Thermostat'][0].Heating_Setpoint_Schedule_Name
-    idf.newidfobject('OUTPUT:VARIABLE')
-    idf.idfobjects['OUTPUT:VARIABLE'][-1].Key_Value = schedule_name
-    idf.idfobjects['OUTPUT:VARIABLE'][-1].Variable_Name = 'Schedule Value'
+    if idf.idfobjects['HVACTemplate:Thermostat']:
+        schedule_name = idf.idfobjects['HVACTemplate:Thermostat'][0].Heating_Setpoint_Schedule_Name
+        idf.newidfobject('OUTPUT:VARIABLE')
+        idf.idfobjects['OUTPUT:VARIABLE'][-1].Key_Value = schedule_name
+        idf.idfobjects['OUTPUT:VARIABLE'][-1].Variable_Name = 'Schedule Value'
 
     # newdir = os.path.basename(__file__).replace('.py','')
     foldername = scriptname.replace('.py', '')
